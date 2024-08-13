05e5b96287e365c6e265c56adec0f2fe4548fe39
Katze <henriquezhector1@hotmail.com>
2024-08-12 21:09:49 -0600

    se agrego la pantalla del podcast con una imagen y controles de reproductor

--- lib/screens/podcast_screen.dart
+++ lib/screens/podcast_screen.dart
@@ -5,6 +5,7 @@ class PodcastScreen extends StatefulWidget {
   const PodcastScreen({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _PodcastScreenState createState() => _PodcastScreenState();
 }
 
